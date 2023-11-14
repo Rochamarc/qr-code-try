04feeaf59862dbadc2d9593ffec05c1a1e3c1ac8
karkraz <rochadcmarcos@gmail.com>
2023-11-14 14:47:30 -0300

    update main file

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
 import qrcode
 
-def gerar_qr_code(link, nome_arquivo):
-    # Cria um objeto QRCode
+def generates_qr_code(link: str, file_name: str) -> None:
+    """Generates an png qr_code file
+    
+    Parameters
+    ----------
+    link : str
+        Link returned by the qr code
+    file_names : str
+        Name of final file with extension. ex: .png, .jpg
+    """
+
+    # Creates QRCode object
     qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4,)
 
-    # Adiciona os dados ao QRCode
+    # Add link to qr code
     qr.add_data(link)
     qr.make(fit=True)
 
-    # Cria uma imagem QRCode
+    # Creates a image
     img = qr.make_image(fill_color="black", back_color="white")
 
-    # Salva a imagem como um arquivo PNG
-    img.save(nome_arquivo)
+
+    img.save(file_name)
 
 if __name__ == "__main__":
-    # Solicita o link do usuário
-    link = input("Digite o link para o QR code: ")
+    # Get the link
+    link = input("Type link to the QR code: ")
     
-    # Define o nome do arquivo de saída
-    nome_arquivo = input("Digite o nome do arquivo de saída (sem extensão): ") + ".png"
+    # Defines file name
+    file_name = input("Type file name  (with extension): ")
 
-    # Gera o QR code e salva como PNG
-    gerar_qr_code(link, nome_arquivo)
+    # Generates the qr code
+    generates_qr_code(link, file_name)
 
-    print(f"O QR code foi gerado e salvo como {nome_arquivo}.")
+    print("QR Code generated successfully;")
